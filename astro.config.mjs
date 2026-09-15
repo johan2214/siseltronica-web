37f43c32acf0a022a7daf5675ad7e598fffd02fc
Anderson Rivas <anderson@rivas.dev>
2026-09-15 16:39:57 -0400

    ajustes vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
+  adapter: vercel(),
   image: {
     domains: ["images.unsplash.com"],
   },
